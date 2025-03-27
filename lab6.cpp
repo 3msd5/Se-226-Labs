3af89b4bb1a082f4d2736f8517004d78c832f679
Muhammed Sait Dokur <107061599+3msd5@users.noreply.github.com>
2025-03-27 14:33:20 +0300

    Update lab6.cpp

--- lab6.cpp
+++ lab6.cpp
@@ -1,3 +1,5 @@
+#################### 5 ####################
+    
 #include <iostream>
 
 using namespace std;
@@ -28,6 +30,42 @@ int main() {
     cin >> num;
 
     double y = harmonic(num);
+    cout<<" = "<<y;
+
+}
+
+#################### 5 ####################
+#include <iostream>
+
+using namespace std;
+double harmonicSum =0;
+
+double harmonic(int n){
+    if(n==0){
+        return 0;
+    }
+    harmonic(n-1);
+    harmonicSum +=1.0/n;
+    if(n==1){
+        cout<<"1";
+    }
+    else{
+        cout<<" + 1/"<<n;
+    }
+    return harmonicSum;
+}
+
+
+double harmonic(){
+    int n;
+    cout << "Enter number for the harmonic series: ";
+    cin >> n;
+    return harmonic(n);
+}
+
+int main() {
+
+    double y = harmonic();
     cout<<" = "<<y;
 
 }

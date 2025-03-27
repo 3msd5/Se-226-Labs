06d6fc8655b7763a4eadb7e0a6dc02157db3d7a9
Muhammed Sait Dokur <107061599+3msd5@users.noreply.github.com>
2025-03-27 15:05:07 +0300

    Update lab6.py

--- lab6.py
+++ lab6.py
@@ -19,6 +19,43 @@ print(f"={factorial(num)}")
 
 ################# 2 ##################
 
+def factorial(x):
+    if x == 0 or x == 1:
+        return 1
+    return x * factorial(x - 1)
+
+def writefactorial(x):
+    if  x > 1:
+        writefactorial(x - 1)
+        print("*",end="")
+    print(x, end="")
+
+num = int(input("Which number do you want to calculate the factorial of?"))
+
+print(f"Factorial calculation steps for {num}: ", end="")
+writefactorial(num)
+print(f"={factorial(num)}")
+
+
+sine_term = lambda n,x: ((-1)**n * x**(2*n+1)) / factorial(2*n+1)
+pi = 3.141592653589793
+def sine_x(x,n):
+    x_rad = x*pi/180
+
+    sine_value = 0
+
+    for i in range(n):
+        sine_value += sine_term(i,x_rad)
+
+    return sine_value
+
+x = float(input("Enter the value of x in degrees: "))
+n = int(input("Enter the number of terms (n) to calculate: "))
+
+result = sine_x(x, n)
+
+print(f"Sine({x}°) using {n} terms of the series is: {result:.6f}")
+
 ################# 3 ##################
 
 harmonicSum=0

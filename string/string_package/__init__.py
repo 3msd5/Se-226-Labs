2b8872d95c1bc21a7e924385515274326e72e627
Muhammed Sait Dokur <107061599+3msd5@users.noreply.github.com>
2025-04-10 14:51:05 +0300

    Create __init__.py

---
+++ string/string_package/__init__.py
@@ -0,0 +1,5 @@
+from .manipulate import reverse_string, capitalize_words, remove_punctuation
+from .stats import count_characters, count_words, average_word_length
+
+if __name__ == "__main__":
+    print("string_package successfully imported!")
